158377e4796ca219cb36b9f5cbf5ca4095112961
Ubuntu <azureuser@build-automation-vm.xigray13tuqerghyrw42xetp1b.bx.internal.cloudapp.net>
2025-11-11 16:25:49 +0000

    tffiles

---
+++ virtualmachine.tf
@@ -0,0 +1,77 @@
+variable "prefix" {
+  default = "kyndryl-boi"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources-balachandar-rg"
+  location = "South India"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm-balachandar"
+  location                        = azurerm_resource_group.example.location
+  resource_group_name             = azurerm_resource_group.example.name
+  size                            = "Standard_B1s"
+  admin_username                  = "docker"
+  admin_password                  = "Docker@12345"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<EOF
+        #!/bin/bash
+        sudo apt-get update
+        sudo apt-get install -y software-properties-common
+        sudo add-apt-repository --yes --update ppa:ansible/ansible
+        sudo apt-get install -y ansible
+        sudo apt-get install -y ssh
+	sudo systemctl restart ssh
+	sudo systemctl enable ssh
+        EOF
+  )
+}
