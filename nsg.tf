6ad1866ce9aec41a64da14c43bf345f3ff50f8e3
Balachandar L <50439996+balachandarln@users.noreply.github.com>
2025-11-13 10:09:22 +0530

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "nsg1" {
-  name                = automationnsg01
+  name                = "automationnsg01"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
